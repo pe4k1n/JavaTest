2cb50564d10b0a6bea20ec1515a2387295b60b33
denHP <po4tadenis@gmail.com>
2016-11-27 13:59:57 +0200

    init2

---
+++ src/dob/specialist/Main.java
@@ -0,0 +1,37 @@
+package dob.specialist;
+/*
+import java.io.BufferedReader;
+import java.io.StringReader;
+*/
+
+import java.io.*;
+
+/**
+ * Created by User on 26.11.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+/*
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(System.in) );
+
+        System.out.print("Какой-то текст на русском. Enter some chars: ");
+        try {
+            String s = reader.readLine();
+
+//            int x = Integer.parseInt(s);
+
+            System.out.println("Yo string is - "+ s );
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
+*/
+    }
+//        BufferedReader reader = new BufferedReader( new StringReader( System.in ));
+//
+//        System.out.println("yo!");
+}
+
