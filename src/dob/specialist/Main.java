4949722399ff4ee4a5ce01562661e1f4b47d2c84
denHP <po4tadenis@gmail.com>
2016-12-04 23:41:26 +0200

    first pro

--- src/dob/specialist/Main.java
+++ src/dob/specialist/Main.java
@@ -3,8 +3,22 @@ package dob.specialist;
 /**
  * Created by User on 27.11.2016.
  */
+
+import java.io.*;
+
 public class Main {
     public static void main(String[] args) {
+        String s;
         System.out.println("YO!");
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Vvedika - ");
+        try {
+            s = reader.readLine();
+            System.out.println("You are entered:"+s);
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
