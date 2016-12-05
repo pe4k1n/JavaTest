b0713eea3d28e7101cf9faa91003142c7905cfdb
denHP <po4tadenis@gmail.com>
2016-12-06 00:23:19 +0200

    package 2

--- src/dob/specialist/Main.java
+++ src/dob/specialist/Main.java
@@ -1,21 +1,24 @@
 package dob.specialist;
 
-/**
- * Created by User on 27.11.2016.
- */
-
 import java.io.*;
+import static java.lang.System.out;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String s;
-        System.out.println("YO!");
+        out.println("YO!");
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.print("Vvedika - ");
+
+        StringBuilder sb = new StringBuilder();
+
+        out.print("Vvedika Имя, сволочь - ");
         try {
             s = reader.readLine();
-            System.out.println("You are entered:"+s);
+//            System.out.println("You are entered:"+s);
+            sb.append("Привет, ");
+            sb.append(s);
+            out.println(sb.toString());
         } catch(IOException e) {
             e.printStackTrace();
         }
---
+++ src/test/Main.java
@@ -0,0 +1,10 @@
+package test;
+
+import java.io.*;
+import static java.lang.System.out;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        out.println("YO!");
+    }
+}
